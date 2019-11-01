4e2fd32d0467fa83860ace5d6108a4f72683884c
Jean-François Corbett <jfcorbett@yahoo.com>
2019-11-01 20:39:31 +0100

    ch11 start

---
+++ app/ch11_migrations/flasktut/pypi_org/data/__all_data_models.py
@@ -0,0 +1,24 @@
+"""
+In this module, import all the other data models
+ so SqlAlchemyBase is made aware of them.
+"""
+
+# Add all your SQLAlchemy models here.
+# This allows us to import just this file when
+# we need to preload the models and ensure they
+# are all loaded.
+
+# noinspection PyUnresolvedReferences
+import pypi_org.data.downloads
+# noinspection PyUnresolvedReferences
+import pypi_org.data.languages
+# noinspection PyUnresolvedReferences
+import pypi_org.data.licenses
+# noinspection PyUnresolvedReferences
+import pypi_org.data.maintainers
+# noinspection PyUnresolvedReferences
+import pypi_org.data.package
+# noinspection PyUnresolvedReferences
+import pypi_org.data.releases
+# noinspection PyUnresolvedReferences
+import pypi_org.data.users
