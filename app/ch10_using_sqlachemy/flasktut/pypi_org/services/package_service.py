1623061baf34de617beecb19930b272c0de7e7c9
Jean-François Corbett <jfcorbett@yahoo.com>
2019-11-01 15:15:48 +0100

    ch10 finish

--- app/ch10_using_sqlachemy/flasktut/pypi_org/services/package_service.py
+++ app/ch10_using_sqlachemy/flasktut/pypi_org/services/package_service.py
@@ -1,14 +1,26 @@
+from typing import List, Optional
+import sqlalchemy.orm as orm
 from pypi_org.data import db_session
 from pypi_org.data.package import Package
 from pypi_org.data.releases import Release
 
 
-def get_latest_packages():
-    return [
-        {'name': 'flask', 'version': '1.2.3'},
-        {'name': 'sqlalchemy', 'version': '2.2.0'},
-        {'name': 'passlib', 'version': '3.0.0'},
-    ]
+def get_latest_releases(limit = 10) -> List[Release]:
+    session = db_session.create_session()
+
+    # joinedload below makes sure each Release's package is
+    # pulled in eagerly, so we don't have to re-query the db
+    # lazily later to figure out what a given release's package is.
+    releases = session.query(Release).\
+        options(orm.joinedload(Release.package)).\
+        order_by(Release.created_date.desc()).\
+        limit(limit).\
+        all()
+
+    # safe to close, because we pulled in everything we need
+    session.close()
+
+    return releases
 
 
 def get_package_count() -> int:
@@ -19,3 +31,18 @@ def get_package_count() -> int:
 def get_release_count() -> int:
     session = db_session.create_session()
     return session.query(Release).count()
+
+
+def get_package_by_id(package_id: str) -> Optional[Package]:
+    if not package_id:
+        return None
+    package_id = package_id.strip().lower()
+
+    session = db_session.create_session()
+    package = session.query(Package).\
+        options(orm.joinedload(Package.releases)).\
+        filter(Package.id == package_id).\
+        first()
+    session.close()
+
+    return package
--- app/ch10_using_sqlachemy/flasktut/pypi_org/views/package_views.py
+++ app/ch10_using_sqlachemy/flasktut/pypi_org/views/package_views.py
@@ -7,9 +7,31 @@ blueprint = flask.Blueprint('packages', __name__, template_folder='templates')
 
 
 @blueprint.route('/project/<package_name>')
-# @response(template_file='packages/details.html')
+@response(template_file='packages/details.html')
 def package_details(package_name: str):
-    return f"Package details for {package_name}"
+    if not package_name:
+        return flask.abort(404)
+
+    package = package_service.get_package_by_id(package_name.strip().lower())
+    if not package:
+        return flask.abort(404)
+
+    latest_version = '0.0.0'
+    latest_release = None
+    is_latest = True
+
+    if package.releases:
+        latest_release = package.releases[0]
+        latest_version = latest_release.version_text
+
+    return {
+        "package": package,
+        "latest_release": latest_release,
+        "release_version": latest_release,
+        "latest_version": latest_version,
+        "is_latest": is_latest,
+    }
+    #return f"Package details for {package.id}"
     # return flask.render_template('home/index.html', packages=test_packages)
 
 @blueprint.route('/<int:rank>')
