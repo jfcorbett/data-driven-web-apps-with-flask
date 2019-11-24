59c27536d70d36b27eb6da44aec3b17fcc7be993
Jean-François Corbett <jfcorbett@yahoo.com>
2019-11-24 22:02:41 +0100

    Fix app dir: flasktut not final

--- app/ch15_deploy/flasktut/server/server_setup.sh
+++ app/ch15_deploy/flasktut/server/server_setup.sh
@@ -52,15 +52,15 @@ cd /apps
 git clone https://github.com/talkpython/data-driven-web-apps-with-flask app_repo
 
 # Setup the web app:
-cd cd /apps/app_repo/app/ch15_deploy/final/
+cd cd /apps/app_repo/app/ch15_deploy/flasktut/
 pip install -r requirements.txt
 
 # Copy and enable the daemon
-cp /apps/app_repo/app/ch15_deploy/final/server/pypi.service /etc/systemd/system/pypi.service
+cp /apps/app_repo/app/ch15_deploy/flasktut/server/pypi.service /etc/systemd/system/pypi.service
 
 systemctl start pypi
 systemctl status pypi
-systemctl enable pypi
+systemctl enable pypi  # daemon will restart automatically after reboot
 
 # Setup the public facing server (NGINX)
 apt install nginx
@@ -68,7 +68,7 @@ apt install nginx
 # CAREFUL HERE. If you are using default, maybe skip this
 rm /etc/nginx/sites-enabled/default
 
-cp /apps/app_repo/app/ch15_deploy/final/server/pypi.nginx /etc/nginx/sites-enabled/pypi.nginx
+cp /apps/app_repo/app/ch15_deploy/flasktut/server/pypi.nginx /etc/nginx/sites-enabled/pypi.nginx
 update-rc.d nginx enable
 service nginx restart
 
